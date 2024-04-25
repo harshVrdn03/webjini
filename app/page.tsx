206030d9a16bcb6f9e9ea7a97c9e72d25415e75b
harshVrdn03 <vk5415308@gmail.com>
2024-04-25 10:16:57 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+// import AboutSectionOne from "@/components/About/AboutSectionOne";
+// import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
